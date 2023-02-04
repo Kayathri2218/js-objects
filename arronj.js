b54b4bc3789439165187c63a52869fcdbec5ea33
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-04 14:49:10 -0500

    adding javascript oject workout.

---
+++ arronj.js
@@ -0,0 +1,424 @@
+// 1. student.
+
+document.write("1. student");
+document.write("<br>");
+document.write("<br>");
+
+let student = {
+    name: "selva",
+    class: 12,
+    age: 18,
+    id: 22,
+    mark: 80,
+    weight: "45kg"
+}
+console.log(student.age);
+console.log(student["weight"]);
+console.log(student);
+document.write(JSON.stringify(student));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 2. car.
+
+document.write("2. Car");
+document.write("<br>");
+document.write("<br>");
+
+let car = {
+    brand: "Audi",
+    model: "some one",
+    color: "blue",
+    design: "race car",
+    milage: 70,
+    price: "2 laks"
+}
+console.log(car.color);
+console.log(car["price"]);
+console.log(car);
+document.write(JSON.stringify(car));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 3. laptop.
+
+document.write("3. Laptop");
+document.write("<br>");
+document.write("<br>");
+
+let laptop = {
+    brand: "Apple",
+    model: "some one",
+    color: "Black",
+    design: "Black shining",
+    processor: "Latest",
+    price: "1 laks"
+}
+console.log(laptop.brand);
+console.log(laptop["processor"]);
+console.log(laptop);
+document.write(JSON.stringify(laptop));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 4. Mobile.
+
+document.write("4. Mobile");
+document.write("<br>");
+document.write("<br>");
+
+let mobile = {
+    brand: "Apple",
+    model: "some one",
+    color: "Black",
+    design: "Black shining",
+    processor: "Latest",
+    price: "1.5 laks"
+}
+console.log(mobile.color);
+console.log(mobile["price"]);
+console.log(mobile);
+document.write(JSON.stringify(mobile));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 5. Home.
+
+document.write("5. Home");
+document.write("<br>");
+document.write("<br>");
+
+let home = {
+    ground: 3,
+    floor: 2,
+    color: "Sky blue",
+    price: "30 laks",
+    place: "tenkasi outer",
+    door: "sandalwood door"
+}
+console.log(home.floor);
+console.log(home["place"]);
+console.log(home);
+document.write(JSON.stringify(home));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 6. Shirt.
+
+document.write("6. Shirt");
+document.write("<br>");
+document.write("<br>");
+
+let shirt = {
+    color: "blue",
+    size: "XL",
+    design: "Formal",
+    price: "600",
+    brand: "Famous",
+    coloth: "Cotton"
+}
+console.log(shirt.coloth);
+console.log(shirt["size"]);
+console.log(shirt);
+document.write(JSON.stringify(shirt));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 7. watch.
+
+document.write("7. Watch");
+document.write("<br>");
+document.write("<br>");
+
+let watch = {
+    color: "Black",
+    size: "Fit",
+    design: "Formal",
+    price: "1000",
+    brand: "Famous",
+    belt: "leather"
+}
+console.log(watch.color);
+console.log(watch["price"]);
+console.log(watch);
+document.write(JSON.stringify(watch));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 8. Shoe.
+
+document.write("8. Shoe");
+document.write("<br>");
+document.write("<br>");
+
+let shoe = {
+    color: "Gray",
+    size: "Fit",
+    design: "Formal",
+    price: "3000",
+    brand: "Famous",
+    useses: "leather"
+}
+console.log(shoe.price);
+console.log(shoe["useses"]);
+console.log(shoe);
+document.write(JSON.stringify(shoe));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 9. Bag.
+
+document.write("9. Bag");
+document.write("<br>");
+document.write("<br>");
+
+let bag = {
+    color: "Gray",
+    size: "Fit",
+    design: "Formal",
+    price: "1000",
+    brand: "Famous",
+    coverOf: "water Proof"
+}
+console.log(bag.coverOf);
+console.log(bag["design"]);
+console.log(bag);
+document.write(JSON.stringify(bag));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 10. Bike.
+
+document.write("10. Bike");
+document.write("<br>");
+document.write("<br>");
+
+let bike = {
+    color: "Black",
+    size: "Larger",
+    design: "Normal",
+    price: "3 laks",
+    brand: "RE",
+    model: "Latest"
+}
+console.log(bike.design);
+console.log(bike["brand"]);
+console.log(bike);
+document.write(JSON.stringify(bike));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 11. Pen.
+
+document.write("11. Pen");
+document.write("<br>");
+document.write("<br>");
+
+let pen = {
+    color: "Black",
+    size: "Fitter",
+    design: "Stylest",
+    price: "200",
+    brand: "Famous",
+    model: "Latest"
+}
+console.log(pen.model);
+console.log(pen["color"]);
+console.log(pen);
+document.write(JSON.stringify(pen));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//12 Tree
+
+document.write("12. Tree");
+document.write("<br>");
+document.write("<br>");
+
+let tree={
+	Name:"apple tree",
+	fruit:"apple",
+	Color:"red",
+	taste:"yemmy",
+    count:10,	
+}
+console.log(tree.fruit);
+console.log(pen["color"]);
+console.log(pen);
+document.write(JSON.stringify(tree));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//13. TV
+
+document.write("13. TV");
+document.write("<br>");
+document.write("<br>");
+
+let tv={
+	Name:"samsung",
+	Color:"black",
+	look:"clear",
+	model:"45 inch",
+}
+console.log(tv.look);
+console.log(tv["Name"]);
+console.log(tv);
+document.write(JSON.stringify(tv));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 14. fridge
+
+document.write("14. Fridge");
+document.write("<br>");
+document.write("<br>");
+
+let fridge={
+	Name:"target",
+    height:170,
+	Color:"black",
+	Process:"cooling",	
+}
+console.log(fridge);
+console.log(fridge["Process"]);
+console.log(fridge);
+document.write(JSON.stringify(fridge));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//15 book
+
+document.write("15. book");
+document.write("<br>");
+document.write("<br>");
+
+let book={
+	Name:"ghost book",
+	pages:40,
+	color:"pink",
+	shape:"square",
+}
+console.log(book);
+console.log(book["pages"]);
+console.log(book);
+document.write(JSON.stringify(book));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//16. pencil
+
+document.write("16. pencil");
+document.write("<br>");
+document.write("<br>");
+
+let pencil={
+	Name:"apsara",
+	shape:"round",
+	Color:"black",
+	rate:3,
+    process:"writing",	
+}
+console.log(pencil);
+console.log(pencil["rate"]);
+document.write(pencil);
+document.write(JSON.stringify(pencil));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 17. Washing Machine.
+
+document.write("17. Washing Machine");
+document.write("<br>");
+document.write("<br>");
+
+let washingmachine={
+	name: "samsung",
+	color: "silver",
+	model: "wa65t4",
+	speed: "700rpm",
+	rate: 12000,
+	weight: "7kg"
+}
+console.log(washingmachine.speed);
+console.log(washingmachine["name"]);
+console.log(washingmachine);
+document.write(JSON.stringify(washingmachine));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 19. Ac.
+
+document.write("19. Ac");
+document.write("<br>");
+document.write("<br>");
+
+let ac = {
+	name: "samsung",
+	color: "white",
+	current: "7.5a",
+	power: "1700w",
+	rate: 36000,
+	weight: "10kg"
+}
+console.log(ac.power);
+console.log(ac["current"]);
+console.log(ac);
+document.write(JSON.stringify(ac));
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// 20. Bat.
+
+document.write("20. Bat");
+document.write("<br>");
+document.write("<br>");
+
+let bat = {
+	name: "mrf",
+	type: "cricket",
+	material: "english willow",
+	color: "multicolor",
+	rate: 1000,
+	weight: "1kg"
+}
+console.log(bat.type);
+console.log(bat["material"]);
+console.log(bat);
+document.write(JSON.stringify(bat));
